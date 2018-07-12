0bbb584ae6963522355bb7831ed281c8d8cf2377
Kaleb Swartz <kalebswartz7@gmail.com>
2018-07-11 22:21:48 -0400

    Removed copied parts from sirepo_bluseky file

--- srw_detector.py
+++ srw_detector.py
@@ -129,48 +129,6 @@ def get_options():
         print('COMPONENT:        ' + data['models']['beamline'][i]['title'])
         get_dict_parameters(data['models']['beamline'][i])
 
-
-import bluesky.preprocessors as bpp
-import bluesky.plan_stubs as bps
-import bluesky.plans as bp
-from bluesky.run_engine import RunEngine
-from bluesky.callbacks import best_effort
-from bluesky.simulators import summarize_plan
-from bluesky.utils import install_qt_kicker
-from bluesky.utils import ProgressBarManager
-
-import databroker
-from databroker import Broker, temp_config
-
-from ophyd.utils import make_dir_tree
-
-from srw_handler import SRWFileHandler
-import matplotlib.pyplot as plt
-
-
-RE = RunEngine({})
-
-bec = best_effort.BestEffortCallback()
-RE.subscribe(bec)
-
-# MongoDB backend:
-# db = Broker.named('local')  # mongodb backend
-# try:
-#     databroker.assets.utils.install_sentinels(db.reg.config, version=1)
-# except:
-#     pass
-
-# Temp sqlite backend:
-db = Broker.from_config(temp_config())
-
-RE.subscribe(db.insert)
-db.reg.register_handler('srw', SRWFileHandler, overwrite=True)
-
-plt.ion()
-install_qt_kicker()
-
-_ = make_dir_tree(2018, base_path='/tmp/data')
-
 get_options()
 component_id = input("Please select component: ")
 spec_id_one = input("Please select specification: ")
