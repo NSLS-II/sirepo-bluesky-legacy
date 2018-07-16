cf3e1a373d161fa013192c406eda3dd55d5c0f43
Kaleb Swartz <kalebswartz7@gmail.com>
2018-07-16 11:26:56 -0400

    Added docstring + naming/formatting changes

--- srw_detector.py
+++ srw_detector.py
@@ -1,4 +1,3 @@
-import sys
 import datetime
 from pathlib import Path
 
@@ -10,7 +9,6 @@ from ophyd.sim import SynAxis, NullStatus, new_uid
 from srw_handler import read_srw_file
 from sirepo_bluesky import SirepoBluesky
 
-
 class SRWDetector(Device):
     """
     Use SRW code based on the value of the motor.
@@ -19,8 +17,29 @@ class SRWDetector(Device):
     ----------
     name : str
         The name of the detector
+    optic_name : str
+        The name of the optic being accessed by Bluesky
+    param0 : str
+        The name of the first parameter of the optic being changed
+    motor0 : Ophyd Component
+        The first Ophyd component being controlled in Bluesky scan
+    field0 : str
+        The name corresponding to motor0 that is shown as axis in Bluesky scan
+    motor1 :
+        The second Ophyd component being controlled in Bluesky scan
+    field1 : str
+        The name corresponding to motor1 that is shown as axis in Bluesky scan
+    param1 : str
+        The name of the second parameter of the optic being changed
+    reg : Databroker register
+    sim_id : str
+        The simulation id corresponding to the Sirepo simulation being run on
+        local server
+    watch_name : str
+        The name of the watchpoint viewing the simulation
+    sirepo_server : str
+        Address that identifies access to local Sirepo server
 
-    TODO: complete doc-string.
     """
     image = Cpt(Signal)
     shape = Cpt(Signal)
@@ -29,14 +48,14 @@ class SRWDetector(Device):
     horizontal_extent = Cpt(Signal)
     vertical_extent = Cpt(Signal)
 
-    def __init__(self, name, cname, spec_name1, spec_name2, motor0, field0, motor1=None, field1=None, reg=None,
-                 sim_id=None, watch_id=None, sirepo_server='http://10.10.10.10:8000',
+    def __init__(self, name, optic_name, param0, motor0, field0, motor1=None, field1=None, param1=None, reg=None,
+                 sim_id=None, watch_name=None, sirepo_server='http://10.10.10.10:8000',
                  **kwargs):
         super().__init__(name=name, **kwargs)
         self.reg = reg
-        self.cname = cname
-        self.spec_name1 = spec_name1
-        self.spec_name2 = spec_name2
+        self.optic_name = optic_name
+        self.param0 = param0
+        self.param1 = param1
         self._motor0 = motor0
         self._motor1 = motor1
         self._field0 = field0
@@ -44,7 +63,7 @@ class SRWDetector(Device):
         self._resource_id = None
         self._result = {}
         self._sim_id = sim_id
-        self.watch_id = watch_id
+        self.watch_name = watch_name
         self._sirepo_server = sirepo_server
         self._hints = None
         assert sim_id, 'Simulation ID must be provided. Currently it is set to {}'.format(sim_id)
@@ -71,18 +90,17 @@ class SRWDetector(Device):
         sim_id = self._sim_id
         sb = SirepoBluesky(self._sirepo_server)
         data = sb.auth('srw', sim_id)
-        element = sb.find_element(data['models']['beamline'], 'title', self.cname)
-        element[self.spec_name1] = x * 1000
-        element[self.spec_name2] = y * 1000
-        watch = sb.find_element(data['models']['beamline'], 'title', self.watch_id)
+        element = sb.find_element(data['models']['beamline'], 'title', self.optic_name)
+        element[self.param0] = x * 1000
+        element[self.param1] = y * 1000
+        watch = sb.find_element(data['models']['beamline'], 'title', self.watch_name)
         watch[self._field0] = x
         data['report'] = 'watchpointReport{}'.format(watch['id'])
         sb.run_simulation()
         with open(srw_file, 'wb') as f:
             f.write(sb.get_datafile())
         ret = read_srw_file(srw_file)
-
-        self.image.put(datum_id)
+         self.image.put(datum_id)
         self.shape.put(ret['shape'])
         self.mean.put(ret['mean'])
         self.photon_energy.put(ret['photon_energy'])
@@ -104,6 +122,7 @@ class SRWDetector(Device):
         self._resource_id = None
         self._result.clear()
 
+
 class Component(Device):
     x = Cpt(SynAxis, delay=0.01)
     y = Cpt(SynAxis, delay=0.02)
@@ -114,7 +133,7 @@ def get_dict_parameters(d):
     for key in d:
         if key not in non_parameters:
             parameters.append(key)
-    print(f'SPECIFICATION:   {parameters} \n')
+            print(f'PARAMETERS:        {parameters} \n')
 
 def get_options():
     sb = SirepoBluesky('http://10.10.10.10:8000')
@@ -122,36 +141,26 @@ def get_options():
     watchpoints = []
     print("Tunable parameters for Bluesky scan: ")
     for i in range(0, len(data['models']['beamline'])):
-        print('COMPONENT:        ' + data['models']['beamline'][i]['title'])
+        print('OPTICAL ELEMENT:    ' + data['models']['beamline'][i]['title'])
         get_dict_parameters(data['models']['beamline'][i])
         if data['models']['beamline'][i]['type'] == 'watch':
             watchpoints.append(data['models']['beamline'][i]['title'] +
             ', position: ' + str(data['models']['beamline'][i]['position']))
     print(f'WATCHPOINTS:       {watchpoints}')
     if len(watchpoints) < 1:
-        print("No watchpoint found. This simulation will not work.")
-        sys.exit()
+        raise ValueError('No watchpoints found. This simulation will not work')
 
-sim_id = input('Please enter sim ID: ')
+sim_id = input("Please enter sim ID: ")
 get_options()
-component_id = input("Please select component: ")
-spec_id_one = input("Please select specification: ")
-spec_id_two = input("Please select another specification or press ENTER to only use one: ")
-watch_id = input("Please select watchpoint: ")
+optic_id = input("Please select optical element: ")
+param0 = input("Please select parameter: ")
+param1 = input("Please select another parameter or press ENTER to only use one: ")
+watch_name = input("Please select watchpoint: ")
 
-c = Component(name=component_id)
-srw_det = SRWDetector(name='srw_det', cname=component_id, spec_name1=spec_id_one,
-                      spec_name2=spec_id_two, motor0=c.x, field0=component_id + '_x',
-                      motor1=c.y, field1=component_id + '_y', reg=db.reg,
-                      sim_id=sim_id, watch_id=watch_id)
+c = Component(name=optic_id)
+srw_det = SRWDetector(name='srw_det', optic_name=optic_id, param0=param0,
+                      param1=param1, motor0=c.x, field0=optic_id + '_x',
+                      motor1=c.y, field1=optic_id + '_y', reg=db.reg,
+                      sim_id=sim_id, watch_name=watch_name)
 srw_det.read_attrs = ['image', 'mean', 'photon_energy']
 srw_det.configuration_attrs = ['horizontal_extent', 'vertical_extent', 'shape']
-
-#Following comments are for testing purposes:
-
-#srw_det.stage()
-#srw_det.trigger()
-#srw_det.unstage()
-
-#RE(bp.grid_scan([srw_det], c.x, 0, 1e-3, 10, c.y, 0, 1e-3, 10, True))
-#RE((bp.scan([srw_det], c.x, 0, 1e-3, 10)))
