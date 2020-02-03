72daa2298f56a85b06bf2067a1a3030196f0ad8c
AbbyGi <abbygi09@gmail.com>
2020-02-03 12:45:59 -0500

    Generalized TES optimization functions

--- run_optimization.py
+++ run_optimization.py
@@ -23,69 +23,48 @@ def ensure_bounds(vec, bounds):
     return vec_new
 
 
-def omea(population, fields):  # needs work to make it generalized
+def omea(population, fields):
     ind_sol = []
     population.sort()
     print('Getting population individual solutions\nProgress:')
+    print(str(1), 'of', str(len(population)))
+    ind_count_params = []
+    for t in range(len(fields)):
+        ind_count_params.append(fields[t])
+        ind_count_params.append(population[0][t])
+    RE(bps.mv(*ind_count_params))
+    RE(bp.count([sirepo_det, *fields]))
+    ind_sol.append(db[-1].table()['sirepo_det_mean'].values[0])
     for i in range(len(population) - 1):
         chk_mean = []
-        # indv_pos = {}
-        print(str(i + 1), 'of', str(len(population)))
-        RE(bp.scan([sirepo_det],
-                   fields[0], population[i][0], population[i + 1][0],
-                   fields[1], population[i][1], population[i + 1][1],
-                   num=3))
-        if i == 0:
-            ind_sol.append(db[-1].table()['sirepo_det_mean'].values[0])
-            ind_sol.append(db[-1].table()['sirepo_det_mean'].values[-1])
-            chk_mean.append(db[-1].table()['sirepo_det_mean'].values[1])
-            chk_mean.extend(db[-1].table()['sirepo_det_mean'].values[2:-1])
-        else:
-            ind_sol.append(db[-1].table()['sirepo_det_mean'].values[-1])
-            chk_mean.extend(db[-1].table()['sirepo_det_mean'].values[1:-1])
-        print()
-        # need to get positions of individuals
-        # then find max mean between population individuals
-        # then replace the next individual with the max in between if it's higher
-        # and update population and ind_sol
+        print(str(i + 2), 'of', str(len(population)))
+        between_linspace = np.linspace(population[i], population[i + 1], 4)
+        between = between_linspace[1:-1]
+        for j in range(len(between)):
+            between_count_params = []
+            for t in range(len(fields)):
+                between_count_params.append(fields[t])
+                between_count_params.append(between[j][t])
+            RE(bps.mv(*between_count_params))
+            RE(bp.count([sirepo_det, *fields]))
+            chk_mean.append(db[-1].table()['sirepo_det_mean'].values[0])
 
-        #for j in range(len(fields)):
-        #    fields[j].set(population[i][j])
-        #RE(bps.mov(field0, population[i][0],
-        #           field1, population[i][1],
-        #           field2, population[i][2],
-        #           field3, population[i][3]))
-        #sirepo_det.trigger()
-        #ind_sol.append(sirepo_det.mean.get())
-    #print('********************')
-
-    # loop through each pair of individuals and look for higher evals
-    #for h in range(len(population) - 1):
-        #if np.all(np.round(population[h], 3) == np.round(population[h + 1], 3)):
-        #    pass
-        #else:
-        #RE(bp.scan([sirepo_det],
-        #            fields[0], population[i][0], population[i + 1][0],
-        #            fields[1], population[i][1], population[i + 1][1],
-        #            num=3))
-        #hold_field0_size = db[-1].table()[fields[0].name].values
-        #hold_field1_size = db[-1].table()[fields[1].name].values
-        #hold_mean = db[-1].table()['sirepo_det_mean'].values
-        #chk_field0_size = hold_field0_size[1:-1]
-        #chk_field1_size = hold_field1_size[1:-1]
-        #chk_mean = hold_mean[1:-1]
-        #chk_params = [chk_field0_size, chk_field1_size]
+        ind_count_params.clear()
+        for t in range(len(fields)):
+            ind_count_params.append(fields[t])
+            ind_count_params.append(population[i + 1][t])
+        RE(bps.mv(*ind_count_params))
+        RE(bp.count([sirepo_det, *fields]))
+        ind_sol.append(db[-1].table()['sirepo_det_mean'].values[0])
 
         # find index of max
-        #ii = np.argmax(chk_mean)
-        # print(chk_field0_size[ii], chk_field1_size[ii], chk_mean[ii])
+        ii = np.argmax(chk_mean)
+        print(between[ii], chk_mean[ii])
 
-        #if chk_mean[ii] > ind_sol[i + 1]:
-        #    ind_sol[i + 1] = chk_mean[ii]
-        #    for k in range(len(population[i])):
-        #        # print(chk_params[k][ii])
-        #        population[i + 1][k] = chk_params[k][ii]
-    print()
+        if chk_mean[ii] > ind_sol[i + 1]:
+            ind_sol[i + 1] = chk_mean[ii]
+            for k in range(len(population[i])):
+                population[i + 1][k] = between[ii][k]
 
     return population, ind_sol
 
@@ -178,26 +157,18 @@ def diff_ev(bounds, fields, popsize=10, crosspb=0.8, mut=0.05, threshold=1.9, mu
     for i in range(len(fields)):
         init_indv.append(fields[i].get()[0])
     population.append(init_indv)
-    #population.append([field0.get()[0], field1.get()[0], field2.get()[0], field3.get()[0]])
     for i in range(popsize - 1):
         indv = []
         for j in range(len(bounds)):
             indv.append(uniform(bounds[j][0], bounds[j][1]))
         population.append(indv)
-
     init_pop = population[:]
-    # plot_pop(fit_func, init_pop, 'Initial population')
 
     # Evaluate fitness
     # OMEA
-    # finds highest evaluation and changes population as new highest eval is discovered
-    # may change this later so population isn't changed until all better evals are discovered
-    # init_pop.sort()
-    new_init_pop, ind_sol = omea(init_pop, fields)  # ***
+    new_init_pop, ind_sol = omea(init_pop, fields)
 
     pop = new_init_pop[:]
-
-    # plot_pop(fit_func, new_init_pop, 'Better initial population')
 
     # Termination conditions
     v = 0  # generation number
@@ -209,12 +180,10 @@ def diff_ev(bounds, fields, popsize=10, crosspb=0.8, mut=0.05, threshold=1.9, mu
             break
         else:
             print('\nGENERATION ' + str(v + 1))
-            print('Working on selection')
+            print('Working on mutation, crossover, and selection')
             gen_scores = []  # score keeping
             best_gen_sol = []  # holding best scores of each generation
             for w in range(popsize):
-                score_trial = []
-                score_target = []
                 # mutation
                 x_t = pop[w]  # target individual
                 if mut_type == 'rand/1':
@@ -239,18 +208,22 @@ def diff_ev(bounds, fields, popsize=10, crosspb=0.8, mut=0.05, threshold=1.9, mu
 
                 # selection
                 print(str(2 * w + 1), 'of', str(popsize * 2))
-                RE(bps.mov(field0, v_trial[0],
-                           field1, v_trial[1]))
-                sirepo_det.trigger()
-                score_trial = sirepo_det.mean.get()
+                count_params = []
+                for t in range(len(fields)):
+                    count_params.append(fields[t])
+                    count_params.append(v_trial[t])
+                RE(bps.mv(*count_params))
+                RE(bp.count([sirepo_det, *fields]))
+                score_trial = db[-1].table()['sirepo_det_mean'].values[0]
 
-                #for i in range(len(x_t)):
-                #    print('**', str(i + 1), 'of', str(len(x_t)))
                 print(str(2 * w + 2), 'of', str(popsize * 2))
-                RE(bps.mov(field0, x_t[0],
-                           field1, x_t[1]))
-                sirepo_det.trigger()
-                score_target = sirepo_det.mean.get()
+                count_params.clear()
+                for t in range(len(fields)):
+                    count_params.append(fields[t])
+                    count_params.append(x_t[t])
+                RE(bps.mv(*count_params))
+                RE(bp.count([sirepo_det, *fields]))
+                score_target = db[-1].table()['sirepo_det_mean'].values[0]
 
                 if score_trial > score_target:
                     pop[w] = v_trial
@@ -259,11 +232,8 @@ def diff_ev(bounds, fields, popsize=10, crosspb=0.8, mut=0.05, threshold=1.9, mu
                     gen_scores.append(score_target)
 
             # score keeping
-            # plot_pop(fit_func, pop, 'Gen ' + str(v + 1) + ' population')
-
             gen_best = max(gen_scores)  # fitness of best individual
             gen_sol = pop[gen_scores.index(max(gen_scores))]  # solution of best individual
-            #gen_worst = min(gen_scores)  # fitness of best individual
             worst_indv = pop[gen_scores.index(min(gen_scores))]  # solution of best individual
             best_gen_sol.append(gen_sol)
 
@@ -282,7 +252,7 @@ def diff_ev(bounds, fields, popsize=10, crosspb=0.8, mut=0.05, threshold=1.9, mu
             if consec_best_ctr >= 5 and old_best_fit_val >= threshold:
                 pass
             else:
-                pop, _ = omea(pop, field0, field1)  # ***
+                pop, _ = omea(pop, fields)  # ***
 
             # introduce a random individual for variation
             for k in range(len(worst_indv)):
@@ -294,31 +264,38 @@ def diff_ev(bounds, fields, popsize=10, crosspb=0.8, mut=0.05, threshold=1.9, mu
 
 
 sirepo_det = sd.SirepoDetector(sim_id='3eP3NeVp', reg=db.reg)
-sirepo_det.select_optic('Aperture1')
-param1 = sirepo_det.create_parameter('horizontalSize')
-param2 = sirepo_det.create_parameter('verticalSize')
-#param3 = sirepo_det.create_parameter('position')
-sirepo_det.select_optic('Aperture2')
-param4 = sirepo_det.create_parameter('horizontalSize')
-param5 = sirepo_det.create_parameter('verticalSize')
-#param6 = sirepo_det.create_parameter('position')
+#sirepo_det.select_optic('Aperture1')
+#param1 = sirepo_det.create_parameter('horizontalSize')
+#param2 = sirepo_det.create_parameter('verticalSize')
+# param3 = sirepo_det.create_parameter('position')
+#sirepo_det.select_optic('Aperture2')
+#param4 = sirepo_det.create_parameter('horizontalSize')
+#param5 = sirepo_det.create_parameter('verticalSize')
+# param6 = sirepo_det.create_parameter('position')
 #sirepo_det.select_optic('Toroid')
 #param7 = sirepo_det.create_parameter('tangentialRadius')
 #param8 = sirepo_det.create_parameter('sagittalRadius')
 #param9 = sirepo_det.create_parameter('tangentialSize')
 #param10 = sirepo_det.create_parameter('sagittalSize')
+#sirepo_det.read_attrs = ['image', 'mean', 'photon_energy']
+#sirepo_det.configuration_attrs = ['horizontal_extent',
+#                                  'vertical_extent',
+#                                  'shape']
+fields = []
+sirepo_det.select_optic('Aperture1')
+fields.append(sirepo_det.create_parameter('horizontalSize'))
+fields.append(sirepo_det.create_parameter('verticalSize'))
+sirepo_det.select_optic('Aperture2')
+fields.append(sirepo_det.create_parameter('horizontalSize'))
+fields.append(sirepo_det.create_parameter('verticalSize'))
 sirepo_det.read_attrs = ['image', 'mean', 'photon_energy']
 sirepo_det.configuration_attrs = ['horizontal_extent',
                                   'vertical_extent',
                                   'shape']
-fields = []
-fields.append(param1)
-fields.append(param2)
-fields.append(param4)
-fields.append(param5)
 # sirepo_det.update_parameters()
 #print()
 #sirepo_det.trigger()
 #print()
 
-diff_ev(bounds=[(3, 5), (.3, .5), (.05, .15), (.05, .15)], fields= fields, popsize=5, crosspb=0.8, mut=0.1, threshold=0, mut_type='best/1')
+diff_ev(bounds=[(3, 5), (.3, .5), (.05, .15), (.05, .15)], fields= fields, popsize=5,
+        crosspb=0.8, mut=0.1, threshold=0, mut_type='best/1')
