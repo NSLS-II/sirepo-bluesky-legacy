57c661236dbe3bc859e7646e59de21078e02f81a
AbbyGi <abbygi09@gmail.com>
2020-02-14 08:52:51 -0500

    Fixed OMEA

--- run_optimization.py
+++ run_optimization.py
@@ -1,7 +1,10 @@
 from re_config import *
 import numpy as np
 from random import random, uniform
+from multiprocessing import Process
+import hashlib
 
+from sirepo_bluesky import SirepoBluesky
 import sirepo_detector as sd
 
 
@@ -19,28 +22,28 @@ def ensure_bounds(vec, bounds):
         # the variable is fine
         if bounds[i][0] <= vec[i] <= bounds[i][1]:
             vec_new.append(vec[i])
-
     return vec_new
 
 
-def omea(population, fields, grazing_params, grazing_index, autocompute_types):
-    ind_sol = []
-    population.sort()
+def omea(positions, fields, grazing_params, grazing_index, autocompute_types):
+    evaluations = []
+    max_positions = []
+    max_evals = []
     print('Getting population individual solutions\nProgress:')
-    print(str(1), 'of', str(len(population)))
+    print(str(1), 'of', str(len(positions)))
     ind_count_params = []
     for t in range(len(fields)):
         ind_count_params.append(fields[t])
-        ind_count_params.append(population[0][t])
+        ind_count_params.append(positions[0][t])
     RE(bps.mv(*ind_count_params))
     if len(grazing_params) > 0:
         update_grazing_vectors(grazing_params, grazing_index, fields, autocompute_types)
     RE(bp.count([sirepo_det, *fields]))
-    ind_sol.append(db[-1].table()['sirepo_det_mean'].values[0])
-    for i in range(len(population) - 1):
+    evaluations.append(db[-1].table()['sirepo_det_mean'].values[0])
+    for i in range(1, len(positions)):
         chk_mean = []
-        print(str(i + 2), 'of', str(len(population)))
-        between_linspace = np.linspace(population[i], population[i + 1], 4)
+        print(str(i + 1), 'of', str(len(positions)))
+        between_linspace = np.linspace(positions[i - 1], positions[i], 4)
         between = between_linspace[1:-1]
         for j in range(len(between)):
             between_count_params = []
@@ -56,23 +59,25 @@ def omea(population, fields, grazing_params, grazing_index, autocompute_types):
         ind_count_params.clear()
         for t in range(len(fields)):
             ind_count_params.append(fields[t])
-            ind_count_params.append(population[i + 1][t])
+            ind_count_params.append(positions[i][t])
         RE(bps.mv(*ind_count_params))
         if len(grazing_params) > 0:
             update_grazing_vectors(grazing_params, grazing_index, fields, autocompute_types)
         RE(bp.count([sirepo_det, *fields]))
-        ind_sol.append(db[-1].table()['sirepo_det_mean'].values[0])
+        evaluations.append(db[-1].table()['sirepo_det_mean'].values[0])
 
         # find index of max
-        ii = np.argmax(chk_mean)
-        print(between[ii], chk_mean[ii])
+        ii = chk_mean.index(np.max(chk_mean))
+        # print(between[ii], chk_mean[ii])
+        max_positions.append(between[ii])
+        max_evals.append(chk_mean[ii])
 
-        if chk_mean[ii] > ind_sol[i + 1]:
-            ind_sol[i + 1] = chk_mean[ii]
-            for k in range(len(population[i])):
-                population[i + 1][k] = between[ii][k]
-
-    return population, ind_sol
+    for i in range(len(max_positions)):
+        if max_evals[i] > evaluations[i + 1]:
+            evaluations[i + 1] = max_evals[i]
+            for k in range(len(positions[i])):
+                positions[i + 1][k] = max_positions[i][k]
+    return positions, evaluations
 
 
 def rand_1(pop, popsize, t_indx, mut, bounds):
@@ -156,8 +161,7 @@ def rand_2(pop, popsize, t_indx, mut, bounds):
     return v_donor
 
 
-def update_grazing_vectors(grazing_params, grazing_index, fields, autocompute_types):  # ******************
-    vectors = []
+def update_grazing_vectors(grazing_params, grazing_index, fields, autocompute_types):
     for i in range(len(grazing_index)):
         grazing_angle = fields[grazing_index[i]].get()[0]
         nvx = nvy = np.sqrt(1 - np.sin(grazing_angle / 1000) ** 2)
@@ -168,7 +172,6 @@ def update_grazing_vectors(grazing_params, grazing_index, fields, autocompute_ty
         elif autocompute_types[i] == 'vertical':
             nvx = tvx = 0
         for j in range(len(grazing_params)):
-            print(5 * i + j)
             if 'normalVectorX' in grazing_params[5 * i + j].name:
                 grazing_params[5 * i + j].set(nvx)
             elif 'normalVectorY' in grazing_params[5 * i + j].name:
@@ -179,16 +182,64 @@ def update_grazing_vectors(grazing_params, grazing_index, fields, autocompute_ty
                 grazing_params[5 * i + j].set(tvy)
             elif 'normalVectorZ' in grazing_params[5 * i + j].name:
                 grazing_params[5 * i + j].set(nvz)
-        print()
 
 
-def diff_ev(bounds, fields, popsize=10, crosspb=0.8, mut=0.05, threshold=1.9, mut_type='rand/1'):
+def mutate(population, strategy, mut, bounds, ind_sol):
+    mutated_indv = []
+    for i in range(len(population)):
+        if strategy == 'rand/1':
+            v_donor = rand_1(population, len(population), i, mut, bounds)
+        elif strategy == 'best/1':
+            v_donor = best_1(population, len(population), i, mut, bounds, ind_sol)
+        elif strategy == 'current-to-best/1':
+            v_donor = current_to_best_1(population, len(population), i, mut, bounds, ind_sol)
+        elif strategy == 'best/2':
+            v_donor = best_2(population, len(population), i, mut, bounds, ind_sol)
+        elif strategy == 'rand/2':
+            v_donor = rand_2(population, len(population), i, mut, bounds)
+        mutated_indv.append(v_donor)
+    return mutated_indv
+
+
+def crossover(population, mutated_indv, crosspb):
+    crossover_indv = []
+    for i in range(len(population)):
+        v_trial = []
+        x_t = population[i]
+        for j in range(len(x_t)):
+            crossover_val = random()
+            if crossover_val <= crosspb:
+                v_trial.append(mutated_indv[i][j])
+            else:
+                v_trial.append(x_t[j])
+        crossover_indv.append(v_trial)
+    return crossover_indv
+
+
+def select(population, crossover_indv, ind_sol, fields, grazing_params, grazing_index, autocompute_types):
+    positions = [elm for elm in crossover_indv]
+    positions.insert(0, population[0])
+    positions, evals = omea(positions, fields, grazing_params,grazing_index, autocompute_types)
+    positions = positions[1:]
+    evals = evals[1:]
+    for i in range(len(evals)):
+        if evals[i] < ind_sol[i]:
+            population[i] = positions[i]
+            ind_sol[i] = evals[i]
+    population.reverse()
+    ind_sol.reverse()
+    return population, ind_sol
+
+
+def diff_ev(bounds, fields, popsize=5, crosspb=0.8, mut=0.05, threshold=1.9, mut_type='rand/1'):
     # Initial population
     population = []
     init_indv = []
     grazing_params = []
     grazing_index = []
     autocompute_types = []
+    best_fitness = [0]
+    count_params = []
     for i in range(len(fields)):
         init_indv.append(fields[i].get()[0])
         if 'grazingAngle' in fields[i].name and ('Toroid' in fields[i].name or 'Circular Cylinder' in
@@ -215,104 +266,105 @@ def diff_ev(bounds, fields, popsize=10, crosspb=0.8, mut=0.05, threshold=1.9, mu
         population.append(indv)
     init_pop = population[:]
 
-    # need to update grazing angle vectors for accurate mean readings
-    # if len(grazing_params) > 0:
-    #    update_grazing_vectors(grazing_params, grazing_index, fields, autocompute_types)
-
-    # Evaluate fitness
-    # OMEA
-    new_init_pop, ind_sol = omea(init_pop, fields, grazing_params, grazing_index, autocompute_types)
-
-    pop = new_init_pop[:]
+    # Evaluate fitness/OMEA
+    init_pop.sort()
+    pop, ind_sol = omea(init_pop, fields, grazing_params, grazing_index, autocompute_types)
+    # reverse pop and ind_sol for OMEA to work better (maybe?)
+    pop.reverse()
+    ind_sol.reverse()
 
     # Termination conditions
     v = 0  # generation number
     consec_best_ctr = 0  # counting successive generations with no change to best value
     old_best_fit_val = 0
     while not (consec_best_ctr >= 5 and old_best_fit_val >= threshold):
-        if v >= 100:
-            print("It's taking too long. Stopping optimization.")
+        print('\nGENERATION ' + str(v + 1))
+        print('Working on mutation, crossover, and selection')
+        # gen_scores = []  # score keeping
+        best_gen_sol = []  # holding best scores of each generation
+        mutated_trial_pop = mutate(pop, mut_type, mut, bounds, ind_sol)
+        cross_trial_pop = crossover(pop, mutated_trial_pop, crosspb)
+        pop, ind_sol = select(pop, cross_trial_pop, ind_sol, fields, grazing_params, grazing_index, autocompute_types)
+
+        gen_best = np.max(ind_sol)
+        best_indv = pop[ind_sol.index(gen_best)]
+        best_gen_sol.append(best_indv)
+        best_fitness.append(gen_best)
+
+        # for w in range(popsize):
+        #     # mutation
+        #     x_t = pop[w]  # target individual
+        #     if mut_type == 'rand/1':
+        #         v_donor = rand_1(pop, popsize, w, mut, bounds)
+        #     elif mut_type == 'best/1':
+        #         v_donor = best_1(pop, popsize, w, mut, bounds, ind_sol)
+        #     elif mut_type == 'current-to-best/1':
+        #         v_donor = current_to_best_1(pop, popsize, w, mut, bounds, ind_sol)
+        #     elif mut_type == 'best/2':
+        #         v_donor = best_2(pop, popsize, w, mut, bounds, ind_sol)
+        #     elif mut_type == 'rand/2':
+        #         v_donor = rand_2(pop, popsize, w, mut, bounds)
+        #
+        #     # crossover
+        #     v_trial = []
+        #     for u in range(len(x_t)):
+        #         crossover = random()
+        #         if crossover <= crosspb:
+        #             v_trial.append(v_donor[u])
+        #         else:
+        #             v_trial.append(x_t[u])
+        #
+        #     # selection
+        #     print(str(2 * w + 1), 'of', str(popsize * 2))
+        #     count_params = []
+        #     for t in range(len(fields)):
+        #         count_params.append(fields[t])
+        #         count_params.append(v_trial[t])
+        #     RE(bps.mv(*count_params))
+        #     if len(grazing_params) > 0:
+        #         update_grazing_vectors(grazing_params, grazing_index, fields, autocompute_types)
+        #     RE(bp.count([sirepo_det, *fields]))
+        #     score_trial = db[-1].table()['sirepo_det_mean'].values[0]
+        #
+        #     print(str(2 * w + 2), 'of', str(popsize * 2))
+        #     count_params.clear()
+        #     for t in range(len(fields)):
+        #         count_params.append(fields[t])
+        #         count_params.append(x_t[t])
+        #     RE(bps.mv(*count_params))
+        #     if len(grazing_params) > 0:
+        #         update_grazing_vectors(grazing_params, grazing_index, fields, autocompute_types)
+        #     RE(bp.count([sirepo_det, *fields]))
+        #     score_target = db[-1].table()['sirepo_det_mean'].values[0]
+        #
+        #     if score_trial > score_target:
+        #         pop[w] = v_trial
+        #         gen_scores.append(score_trial)
+        #     else:
+        #         gen_scores.append(score_target)
+        #
+        # # score keeping
+        # gen_best = max(gen_scores)  # fitness of best individual
+        # gen_sol = pop[gen_scores.index(max(gen_scores))]  # solution of best individual
+        # best_gen_sol.append(gen_sol)
+
+        print('      > BEST FITNESS:', gen_best)
+        print('         > BEST POSITIONS:', best_indv)
+
+        v += 1
+        if np.round(gen_best, 3) == np.round(old_best_fit_val, 3):
+            consec_best_ctr += 1
+            print('Counter:', consec_best_ctr)
+        else:
+            consec_best_ctr = 0
+        old_best_fit_val = gen_best
+
+        if consec_best_ctr >= 5 and old_best_fit_val >= threshold:
+            print('Finished')
             break
         else:
-            print('\nGENERATION ' + str(v + 1))
-            print('Working on mutation, crossover, and selection')
-            gen_scores = []  # score keeping
-            best_gen_sol = []  # holding best scores of each generation
-            for w in range(popsize):
-                # mutation
-                x_t = pop[w]  # target individual
-                if mut_type == 'rand/1':
-                    v_donor = rand_1(pop, popsize, w, mut, bounds)
-                elif mut_type == 'best/1':
-                    v_donor = best_1(pop, popsize, w, mut, bounds, ind_sol)
-                elif mut_type == 'current-to-best/1':
-                    v_donor = current_to_best_1(pop, popsize, w, mut, bounds, ind_sol)
-                elif mut_type == 'best/2':
-                    v_donor = best_2(pop, popsize, w, mut, bounds, ind_sol)
-                elif mut_type == 'rand/2':
-                    v_donor = rand_2(pop, popsize, w, mut, bounds)
-
-                # crossover
-                v_trial = []
-                for u in range(len(x_t)):
-                    crossover = random()
-                    if crossover <= crosspb:
-                        v_trial.append(v_donor[u])
-                    else:
-                        v_trial.append(x_t[u])
-
-                # selection
-                print(str(2 * w + 1), 'of', str(popsize * 2))
-                count_params = []
-                for t in range(len(fields)):
-                    count_params.append(fields[t])
-                    count_params.append(v_trial[t])
-                RE(bps.mv(*count_params))
-                if len(grazing_params) > 0:
-                    update_grazing_vectors(grazing_params, grazing_index, fields, autocompute_types)
-                RE(bp.count([sirepo_det, *fields]))
-                score_trial = db[-1].table()['sirepo_det_mean'].values[0]
-
-                print(str(2 * w + 2), 'of', str(popsize * 2))
-                count_params.clear()
-                for t in range(len(fields)):
-                    count_params.append(fields[t])
-                    count_params.append(x_t[t])
-                RE(bps.mv(*count_params))
-                if len(grazing_params) > 0:
-                    update_grazing_vectors(grazing_params, grazing_index, fields, autocompute_types)
-                RE(bp.count([sirepo_det, *fields]))
-                score_target = db[-1].table()['sirepo_det_mean'].values[0]
-
-                if score_trial > score_target:
-                    pop[w] = v_trial
-                    gen_scores.append(score_trial)
-                else:
-                    gen_scores.append(score_target)
-
-            # score keeping
-            gen_best = max(gen_scores)  # fitness of best individual
-            gen_sol = pop[gen_scores.index(max(gen_scores))]  # solution of best individual
-            best_gen_sol.append(gen_sol)
-
-            print('      > GENERATION BEST:', gen_best)
-            print('         > BEST SOLUTION:', gen_sol)
-
-            v += 1
-            if np.round(gen_best, 3) == np.round(old_best_fit_val, 3):
-                consec_best_ctr += 1
-                print('Counter:', consec_best_ctr)
-            else:
-                consec_best_ctr = 0
-            old_best_fit_val = gen_best
-
-            # OMEA
-            if consec_best_ctr >= 5 and old_best_fit_val >= threshold:
-                pass
-            else:
-                pop, ind_sol = omea(pop, fields, grazing_params, grazing_index, autocompute_types)
-
             # introduce a random individual for variation
+            # this needs OMEA - from current position to replaced individual
             change_index = ind_sol.index(min(ind_sol))
             changed_indv = pop[change_index]
             for k in range(len(changed_indv)):
@@ -331,6 +383,11 @@ def diff_ev(bounds, fields, popsize=10, crosspb=0.8, mut=0.05, threshold=1.9, mu
     print('\nThe best individual is', x_best, 'with a fitness of', gen_best)
     print('It took', v, 'generations')
 
+    # plot best fitness
+    plot_index = np.arange(len(best_fitness))
+    plt.figure()
+    plt.plot(plot_index, best_fitness)
+
 
 sirepo_det = sd.SirepoDetector(sim_id='3eP3NeVp', reg=db.reg)
 
@@ -338,8 +395,8 @@ field_list = []
 sirepo_det.select_optic('Toroid')
 field_list.append(sirepo_det.create_parameter('tangentialRadius'))
 field_list.append(sirepo_det.create_parameter('grazingAngle'))
-sirepo_det.select_optic('Circular Cylinder')
-field_list.append(sirepo_det.create_parameter('grazingAngle'))
+# sirepo_det.select_optic('Circular Cylinder')
+# field_list.append(sirepo_det.create_parameter('grazingAngle'))
 # sirepo_det.select_optic('Elliptical Cylinder')
 # field_list.append(sirepo_det.create_parameter('grazingAngle'))
 sirepo_det.read_attrs = ['image', 'mean', 'photon_energy']
@@ -347,5 +404,7 @@ sirepo_det.configuration_attrs = ['horizontal_extent',
                                   'vertical_extent',
                                   'shape']
 
-diff_ev(bounds=[(1000, 10000), (5, 10), (5, 10)], fields=field_list, popsize=5,
-        crosspb=0.8, mut=0.1, threshold=0, mut_type='best/1')
+# multiprocessing?
+
+diff_ev(bounds=[(1000, 10000), (5, 10)], fields=field_list, popsize=5,
+        crosspb=0.8, mut=0.1, threshold=0, mut_type='rand/1')
